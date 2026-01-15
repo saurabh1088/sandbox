59517acf39a56e485d97c442120398826850fdd2
Saurabh Verma <saurabh1088@gmail.com>
2026-01-15 15:37:26 +0530

    A simple bare minimum structure for executing Swift code.

---
+++ swift/Main.swift
@@ -0,0 +1,15 @@
+//
+//  Main.swift
+//  
+//
+//  Created by Saurabh Verma on 15/01/26.
+//
+
+import Foundation
+
+func kickStarter() {
+    print("Hello, Sandbox!")
+    print("Here we start!")
+}
+
+kickStarter()
