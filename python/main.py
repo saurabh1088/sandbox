de9e2e51b7970ef17ee8b36152d759bbb581ada4
Saurabh Verma <saurabh1088@gmail.com>
2026-01-16 19:38:22 +0530

    Added group python for setting up Python scratchpad.

---
+++ python/main.py
@@ -0,0 +1,11 @@
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(array[0])
+print(array[1])
+print(array[2])
+print(array[3])
+print(array[4])
+print(array[5])
+print(array[6])
+print(array[7])
+print(array[8])
+print(array[9])
