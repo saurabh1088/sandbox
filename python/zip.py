4ac9fb26ab9deea6de92833da681686f5b8325bd
Saurabh Verma <saurabh1088@gmail.com>
2026-02-08 18:41:59 +0530

    Python practice session three - zip - 8th February 2026

--- python/zip.py
+++ python/zip.py
@@ -18,3 +18,19 @@ print(list(combined_uneven))
 
 list_one = [1, 2, 3]
 list_two = [1, 2, 3]
+
+# creating a dictionary using zip
+list_keys = ["one", "two", "three"]
+list_values = [1, 2, 3]
+dictionary_from_zip = dict(zip(list_keys, list_values))
+print(dictionary_from_zip)
+
+def add_item(item, items=[]):
+    items.append(item)
+    return items
+
+print(add_item("apple"))
+print(add_item("banana"))
+print(add_item("orange"))
+# Here the output will be ['apple', 'banana', 'orange'] because the default list is shared across all calls to the function.
+# To avoid this, we can use None as the default value and create a new list inside the function if items is None.
