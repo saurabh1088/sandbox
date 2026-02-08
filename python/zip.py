b263dfe7cb01050da17bbb3aa828a16f759c2434
Saurabh Verma <saurabh1088@gmail.com>
2026-02-08 18:20:05 +0530

    Python practice session two - zip - 8th February 2026

--- python/zip.py
+++ python/zip.py
@@ -4,3 +4,17 @@ list_b = ["Bruce Wayne", "Clark Kent", "Diana Prince"]
 # Using zip to combine the two lists
 combined = zip(list_a, list_b)
 print(list(combined))
+
+list_c = ["Gotham", "Metropolis", "Themyscira"]
+# Using zip to combine three lists
+combined_three = zip(list_a, list_b, list_c)
+print(list(combined_three))
+
+# uneven lists
+list_d = ["Robin", "Supergirl"]
+# Using zip with uneven lists
+combined_uneven = zip(list_a, list_b, list_d)
+print(list(combined_uneven))
+
+list_one = [1, 2, 3]
+list_two = [1, 2, 3]
