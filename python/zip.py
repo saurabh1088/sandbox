5cd9bfda6702c6e38f25ab6a671ac8909566a927
Saurabh Verma <saurabh1088@gmail.com>
2026-02-08 16:45:29 +0530

    Python practice session one - zip - 8th February 2026

---
+++ python/zip.py
@@ -0,0 +1,6 @@
+list_a = ["Batman", "Superman", "Wonder Woman"]
+list_b = ["Bruce Wayne", "Clark Kent", "Diana Prince"]
+
+# Using zip to combine the two lists
+combined = zip(list_a, list_b)
+print(list(combined))
