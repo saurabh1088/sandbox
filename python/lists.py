ddaa0529bef07111b1fb3ec894fc0efee1aaf01a
Saurabh Verma <saurabh1088@gmail.com>
2026-02-17 22:31:38 +0530

    Python practice session two - List comprehension - 17th February 2026

--- python/lists.py
+++ python/lists.py
@@ -1,5 +1,5 @@
 # --- List example one ---
-list_example_one = [1, 2, 3, 4, 5]
+list_example_one = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 reference_list_example_one = list_example_one
 
 print("List referece example show that both variables point to the same list in memory")
@@ -23,3 +23,14 @@ sample_data = ["apple", "bat", "car", "elephant", "dog"]
 list_example_four = [x for x in sample_data if len(x) > 4]
 print("\nList comprehension example to filter words longer than 4 characters")
 print("list_example_four: ", list_example_four)
+
+list_example_five = [x.upper() for x in sample_data]
+print("\nList comprehension example to convert all words to uppercase")
+print("list_example_five: ", list_example_five)
+
+
+sample_long_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list_example_six = ["Even" if x % 2 == 0 else "Odd" for x in sample_long_list]
+print("\nList comprehension example to label numbers as 'Even' or 'Odd'")
+print("list_example_six: ", list_example_six)
+
